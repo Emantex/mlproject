6337ebeeb33a7a2f09445a62df841849dabb73c4
Emmantex <omanoyeemmanuel@gmail.com>
2024-12-04 23:46:26 +0100

    update setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='mlproject',
-    version='0.0.1'
-    author='Emmanuel'
+    version='0.0.1',
+    author='Emmanuel',
     author_email='omanoyeemmanuel@gmail.com',
     package_data=find_packages(),
     install_requires=['pandas', 'seaborn', 'numpy']
